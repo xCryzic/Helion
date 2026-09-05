9d2c1c01b290d5694d9914901f714e4b63554206
xCryzic <lakshit4597@gmail.com>
2026-09-05 10:10:51 +0530

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Helion/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8787',
-      },
-    },
-  },
 })
